b2372ffe48b700ac08d6de3c8a39822b116dec68
Jim Vacca <33520581+MSFTJim@users.noreply.github.com>
2024-01-08 12:41:40 +0000

    Add random selection of fruits and names

--- random8/Program.cs
+++ random8/Program.cs
@@ -1,13 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+Random random = new Random();
+
 string[] fruits = { "apple", "banana", "cherry", "date", "elderberry" };
 int numFruits = 3;
-Random random = new Random();
 string[] randomFruits = random.GetItems(fruits, numFruits);
+
 Console.WriteLine($"Randomly selected {numFruits} fruits: {{ {string.Join(", ", randomFruits)} }}");
 
 string[] names = ["John", "Mary", "Bob","Jim", "Tim","Rick"];
 int numNames = 25;
 var randomNames = random.GetItems(names,25);
 random.Shuffle(randomNames);
+
 Console.WriteLine($"Randomly selected {numNames} names: {{ {string.Join(", ", randomNames)} }}");
