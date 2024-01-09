57929803f2ae829d8dbbfd682e71436db539c3d9
Jim Vacca <33520581+MSFTJim@users.noreply.github.com>
2024-01-09 18:43:48 +0000

    Add DI and import DepenLib

--- KeyConcepts/KeyConcepts/Program.cs
+++ KeyConcepts/KeyConcepts/Program.cs
@@ -1,5 +1,6 @@
 using KeyConcepts.Client.Pages;
 using KeyConcepts.Components;
+using DepenLib;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddSingleton<DemoDependency>();
 
 var app = builder.Build();
 
