85d30ca433cf29e962271299eabc6e34f4f9d41a
Jim Vacca <33520581+MSFTJim@users.noreply.github.com>
2024-02-09 23:18:28 +0000

    Add validation to PersonModel and MyForm

--- FormDemo/FormDemo.Client/Models/PersonModel.cs
+++ FormDemo/FormDemo.Client/Models/PersonModel.cs
@@ -1,8 +1,15 @@
-﻿namespace FormDemo.Client.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FormDemo.Client.Models;
 
 public class PersonModel
 {
+    [Required]
+    [MinLength(5, ErrorMessage = "First name not long enough")]
     public string? FirstName { get; set; }
+
+    [Required(ErrorMessage = "Last name is required")]
+    [MinLength(8, ErrorMessage = "Last name must be at least 8 characters long")]
     public string? LastName { get; set; }
 
 }
